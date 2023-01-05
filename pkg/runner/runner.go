9e6989f62984b89dcd8d93d643c3438c00d5a8eb
Lilla Vass <vLia@users.noreply.github.com>
2023-01-05 14:33:59 +0100

    feat: improved logs (#24)
    * feat: verbose logs

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -11,6 +11,7 @@ import (
 	"github.com/kubeshop/testkube/pkg/executor/content"
 	"github.com/kubeshop/testkube/pkg/executor/output"
 	"github.com/kubeshop/testkube/pkg/executor/runner"
+	"github.com/kubeshop/testkube/pkg/ui"
 )
 
 type Params struct {
@@ -20,6 +21,8 @@ type Params struct {
 	Location        string // RUNNER_LOCATION
 	Token           string // RUNNER_TOKEN
 	Ssl             bool   // RUNNER_SSL
+	GitUsername     string // RUNNER_GITUSERNAME
+	GitToken        string // RUNNER_GITTOKEN
 }
 
 // NewRunner creates init runner
@@ -39,15 +42,21 @@ type InitRunner struct {
 
 // Run prepares data for executor
 func (r *InitRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
+	output.PrintEvent(fmt.Sprintf("%s Initializing...", ui.IconTruck))
 	var params Params
+
+	output.PrintEvent(fmt.Sprintf("%s Reading environment variables for runner setup...", ui.IconWorld))
 	err = envconfig.Process("runner", &params)
 	if err != nil {
-		return result, fmt.Errorf("could not read environment variables: %w", err)
+		return result, fmt.Errorf("%s Could not read environment variables: %w", ui.IconCross, err)
 	}
 
-	gitUsername := os.Getenv("RUNNER_GITUSERNAME")
-	gitToken := os.Getenv("RUNNER_GITTOKEN")
-	if gitUsername != "" || gitToken != "" {
+	gitUsername := params.GitUsername
+	gitToken := params.GitToken
+	output.PrintEvent(fmt.Sprintf("%s Environment variables read successfully", ui.IconCheckMark))
+	printParams(params)
+
+	if gitUsername != "" && gitToken != "" {
 		if execution.Content != nil && execution.Content.Repository != nil {
 			execution.Content.Repository.Username = gitUsername
 			execution.Content.Repository.Token = gitToken
@@ -55,26 +64,33 @@ func (r *InitRunner) Run(execution testkube.Execution) (result testkube.Executio
 	}
 
 	if execution.VariablesFile != "" {
-		filename := "params-file"
-		if err = os.WriteFile(filepath.Join(r.dir, filename), []byte(execution.VariablesFile), 0666); err != nil {
-			return result, err
+		output.PrintEvent(fmt.Sprintf("%s Creating variables file...", ui.IconWorld))
+		file := filepath.Join(r.dir, "params-file")
+		if err = os.WriteFile(file, []byte(execution.VariablesFile), 0666); err != nil {
+			return result, fmt.Errorf("%s Could not create variables file %s: %w", ui.IconCross, file, err)
 		}
+		output.PrintEvent(fmt.Sprintf("%s Variables file created", ui.IconCheckMark))
 	}
 
+	output.PrintEvent(fmt.Sprintf("%s Fetching test content from %s...", ui.IconBox, execution.Content.Type_))
 	path, err := r.Fetcher.Fetch(execution.Content)
 	if err != nil {
-		return result, err
+		return result, fmt.Errorf("could not fetch test content: %w", err)
 	}
+	output.PrintEvent(fmt.Sprintf("%s Test content fetched to path %s", ui.IconCheckMark, path))
 
 	// add copy files in case object storage is set
 	if params.Endpoint != "" {
+		output.PrintEvent(fmt.Sprintf("%s Fetching uploads from object store %s...", ui.IconFile, params.Endpoint))
 		fp := content.NewCopyFilesPlacer(params.Endpoint, params.AccessKeyID, params.SecretAccessKey, params.Location, params.Token, params.Ssl)
 		err = fp.PlaceFiles(execution.TestName, execution.BucketName)
 		if err != nil {
 			output.PrintLog(fmt.Sprintf("could not place bucket: %s", err.Error()))
 		}
+		output.PrintEvent(fmt.Sprintf("%s Placing uploads succeeded.", ui.IconCheckMark))
 	}
 
+	output.PrintEvent(fmt.Sprintf("%s Setting up access to files in %s", ui.IconFile, r.dir))
 	_, err = executor.Run(r.dir, "chmod", nil, []string{"-R", "777", "."}...)
 	if err != nil {
 		output.PrintLog(fmt.Sprintf("could not chmod for data dir: %s", err.Error()))
@@ -86,13 +102,34 @@ func (r *InitRunner) Run(execution testkube.Execution) (result testkube.Executio
 			output.PrintLog(fmt.Sprintf("could not chmod for artifacts dir: %s", err.Error()))
 		}
 	}
+	output.PrintEvent(fmt.Sprintf("%s Access to files enabled", ui.IconCheckMark))
 
-	output.PrintLog("created content path: " + path)
-
+	output.PrintEvent(fmt.Sprintf("%s Initialization successful", ui.IconCheckMark))
 	return testkube.NewPendingExecutionResult(), nil
 }
 
 // GetType returns runner type
 func (r *InitRunner) GetType() runner.Type {
 	return runner.TypeInit
+}
+
+// printParams shows the read parameters in logs
+func printParams(params Params) {
+	output.PrintLog(fmt.Sprintf("RUNNER_ENDPOINT=\"%s\"", params.Endpoint))
+	printSensitiveParam("RUNNER_ACCESSKEYID", params.AccessKeyID)
+	printSensitiveParam("RUNNER_SECRETACCESSKEY", params.SecretAccessKey)
+	output.PrintLog(fmt.Sprintf("RUNNER_LOCATION=\"%s\"", params.Location))
+	printSensitiveParam("RUNNER_TOKEN", params.Token)
+	output.PrintLog(fmt.Sprintf("RUNNER_SSL=%t", params.Ssl))
+	output.PrintLog(fmt.Sprintf("RUNNER_GITUSERNAME=\"%s\"", params.GitUsername))
+	printSensitiveParam("RUNNER_GITTOKEN", params.GitToken)
+}
+
+// printSensitiveParam shows in logs if a parameter is set or not
+func printSensitiveParam(name string, value string) {
+	if len(value) == 0 {
+		output.PrintLog(fmt.Sprintf("%s=\"\"", name))
+	} else {
+		output.PrintLog(fmt.Sprintf("%s=\"********\"", name))
+	}
 }
